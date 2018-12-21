28f1f9c4bf7661063ac8dd80901832efaac8fc9a
yokots <hd.wang1993@gmail.com>
2018-12-21 17:19:55 +0800

    feat: update angular rules

--- rules/angular.js
+++ rules/angular.js
@@ -1,6 +1,7 @@
 module.exports = {
   "extends": ["codelyzer"],
   "rules": {
+    "max-inline-declarations": true,
     "banana-in-box": true,
     "no-forward-ref": true,
     "templates-no-negated-async": true,
@@ -9,7 +10,9 @@ module.exports = {
 
     "decorator-not-allowed": true,
     "no-attribute-parameter-decorator": true,
+    "no-queries-parameter": true,
     "use-host-property-decorator": true,
+    "prefer-inline-decorator": true,
 
     "use-input-property-decorator": true,
     "use-output-property-decorator": true,
@@ -17,6 +20,8 @@ module.exports = {
     "no-output-rename": true,
     "no-output-on-prefix": true,
     "no-output-named-after-standard-event": true,
+    "prefer-output-readonly": true,
+    "no-conflicting-life-cycle-hooks": true,
 
     "contextual-life-cycle": true,
     "use-life-cycle-interface": true,
@@ -28,6 +33,7 @@ module.exports = {
     "import-destructuring-spacing": true,
     "angular-whitespace": [true, "check-interpolation", "check-pipe", "check-semicolon"],
 
+    "enforce-component-selector": true,
     "component-class-suffix": true,
     "directive-class-suffix": true,
   }
